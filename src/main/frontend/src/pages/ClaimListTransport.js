702c277e43df05b5d5b330e46507f330b7294685
solfany <solfany1999@gmail.com>
2023-10-16 12:45:37 +0900

    feat : #6 front-end 더미 data 추가
    접수된 클레임 목록을 보여주는데 테이블 형식으로 보여줄 예정으로 더미 데이터를 삽입하여 정상적으로 출력되는지 확인되도록 작업했다.
    서버 작업 후 더미 데이터는 삭제 예정이다.

---
+++ src/main/frontend/src/pages/ClaimListTransport.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Button, Col, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
+
+function ClaimListTransport() {
+  const navigate = useNavigate();
+
+  // 임시 데이터를 claims 상태에 추가
+  const [claims, setClaims] = useState([
+    {
+      id: 1,
+      orderNumber: "1234",
+      claimType: "Damage",
+      description: "Broken item",
+      claimAmount: "100",
+      responsibleParty: "Carrier",
+    },
+    // 추가적으로 다른 데이터도 넣을 수 있습니다.
+  ]);
+
+  const handleClaimClick = () => {
+    navigate(
+      `/claims/claimlisttransport/transportresponsibilitypage/${claims[0].id}`
+    );
+  };
+
+  const handleRowClick = (claimId) => {
+    navigate(`/claims/claimdetail/${claimId}`);
+  };
+
+  return (
+    <Container className="mt-4 Container">
+      {/* ... 폼 부분 생략 ... */}
+      <h3 className="mt-4">접수 된 운송사 클레임 목록</h3>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Order Number</th>
+            <th>Claim Type</th>
+            <th>Description</th>
+            <th>Claim Amount</th>
+            <th>Responsible Party</th>
+          </tr>
+        </thead>
+        <tbody>
+          {claims.map((claim) => (
+            <tr
+              key={claim.id}
+              onClick={() => handleRowClick(claim.id)}
+              style={{ cursor: "pointer" }}
+            >
+              <td>{claim.orderNumber}</td>
+              <td>{claim.claimType}</td>
+              <td>{claim.description}</td>
+              <td>{claim.claimAmount}</td>
+              <td>{claim.responsibleParty}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <br />
+      <Button onClick={handleClaimClick}>클레임 접수하기</Button>
+    </Container>
+  );
+}
+
+export default ClaimListTransport;
